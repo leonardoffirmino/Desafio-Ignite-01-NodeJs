f96dfefc108d34d3290c4fac6a9615f15d94b765
leonardoffirmino <firmino750@gmail.com>
2021-09-15 22:57:33 -0300

    Route-adjustment-and-readme-addition

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+### Desafio 01 - Conceitos de node - Rocketseat - Ignite
+
+
+## Projeto
+
+Projeto desenvolvido com finalidade de realizar um Todo-list,onde criamos um usuário e a lista de tarefas.Assim atuando com o CRUD padrão no Node.
+
+### Itens abordados no projeto
+
+* Fluxo de rotas
+* Tratamento de erros
+* CRUD 
+* Testes Automatizados
--- src/index.js
+++ src/index.js
@@ -1,41 +1,135 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
+//Middleware
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error:"User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
+
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name,username} = request.body;
+
+  const userExist = users.find(user => user.username === username);
+
+  if(userExist) {
+    return response.status(400).json({error:"Username already exists"});
+  }
+
+  const  user ={
+    id:uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+
+  users.push(user);
+
+  return response.status(201).json(user);
+ 
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+app.get('/todos', checksExistsUserAccount,(request, response) => {
+  const {user} = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+  const {user} = request;
+
+  const {title,deadline} = request.body;
+
+
+  const registerTask = {
+    id:uuidv4(),
+    title,
+    done:false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(registerTask);
+
+  return response.status(201).json(registerTask);
+
+ });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+   const {user} = request;
+   
+   const {title,deadline} = request.body;
+
+
+   const {id} =request.params;
+
+   const todo = user.todos.find(todo=> todo.id === id);
+
+   if(!todo){
+     return response.status(404).json({error:"Todo not found!"})
+   }
+
+   todo.title = title;
+   todo.deadline = new Date(deadline);
+
+   return response.status(200).json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} =request;
+  
+  const {id} = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+
+  if(!todo){
+    return response.status(404).json({error: "Todo Not Found!"});
+  };
+
+  todo.done = true;
+  
+
+  return response.json(todo);
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+
+  const {id} = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if(todoIndex === -1){
+    return response.status(404).json({error:"Todo Not Found!"})
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
+  
 });
 
 module.exports = app;
